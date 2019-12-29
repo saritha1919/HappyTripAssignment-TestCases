1de353dfd97e44b2f20d98aa545087ca1d117009
saritha reddy <saritha.reddy@Pratian.com>
2019-12-29 16:59:45 +0530

    updated bat file test ng name

--- testNGBatchfile.bat
+++ testNGBatchfile.bat
@@ -1,5 +1,5 @@
 set projectLocation=C:\Program Files (x86)\Jenkins\workspace\HappyTrip-testcases
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG testng.xml
+java org.testng.TestNG testsuite.xml
 pause
