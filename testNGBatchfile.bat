3c3d3e1d0a8c61e40548ccd6b57971b9f6b88c6e
saritha reddy <saritha.reddy@Pratian.com>
2019-12-29 16:58:05 +0530

    updated bat file project path

--- testNGBatchfile.bat
+++ testNGBatchfile.bat
@@ -1,4 +1,4 @@
-set projectLocation=D:\Saritha\Saritha\seleniumTraining\GIT-Selenium\HappyTripTestCases
+set projectLocation=C:\Program Files (x86)\Jenkins\workspace\HappyTrip-testcases
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 java org.testng.TestNG testng.xml
